91fe001cd4c6fa39771cd2f2798bb7cb3db6df96
Lars Vinther <lars.k.vinther@gmail.com>
2021-06-07 23:16:03 +0100

    Solved LongestSubstring from Leetcode

---
+++ 03. LongestSubstring/LongestSubstring/LongestSubstring.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestSubstring
+{
+    public class GetLongestSubstring
+    {
+        public int LengthOfLongestSubstring(string inputString)
+        {
+            var charsInCurrentString = GetNewHashSet();
+
+            int lengthOfLongestStringSoFar = 0;
+            foreach (var character in inputString)
+            {
+                if (charsInCurrentString.Contains(character))
+                {
+                    lengthOfLongestStringSoFar = PotentiallyUpdateLongestStringLength(charsInCurrentString, lengthOfLongestStringSoFar);
+                    charsInCurrentString = GetNewHashSet();
+                }
+                charsInCurrentString.Add(character);
+            }
+
+            lengthOfLongestStringSoFar = PotentiallyUpdateLongestStringLength(charsInCurrentString, lengthOfLongestStringSoFar);
+
+
+            return lengthOfLongestStringSoFar;
+        }
+
+        private int PotentiallyUpdateLongestStringLength(HashSet<char> currentSet, int lengthOfLongestStringSoFar)
+        {
+            if (currentSet.Count > lengthOfLongestStringSoFar)
+            {
+                return currentSet.Count;
+            }
+            return lengthOfLongestStringSoFar;
+        }
+
+        private HashSet<char> GetNewHashSet()
+        {
+            return new HashSet<char>();
+        }
+    }
+}
---
+++ 03. LongestSubstring/Tests/Tests.cs
@@ -0,0 +1,33 @@
+using LongestSubstring;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        private GetLongestSubstring _longestSubstring;
+        [SetUp]
+        public void Setup()
+        {
+            _longestSubstring = new GetLongestSubstring();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, _longestSubstring.LengthOfLongestSubstring("abcabcbb"));
+            Assert.AreEqual(1, _longestSubstring.LengthOfLongestSubstring("bbbbb"));
+            Assert.AreEqual(3, _longestSubstring.LengthOfLongestSubstring("pwwkew"));
+            Assert.AreEqual(0, _longestSubstring.LengthOfLongestSubstring(""));
+        }
+
+        [Test]
+        public void CustomTests()
+        {
+            Assert.AreEqual(12, _longestSubstring.LengthOfLongestSubstring("123    -'¨3qwerty:"));
+            Assert.AreEqual(8, _longestSubstring.LengthOfLongestSubstring("/%3!2¤5hhffddss1123"));
+            Assert.AreEqual(12, _longestSubstring.LengthOfLongestSubstring("zxcvb00?´`'-_:.;,@"));
+        }
+    }
+}
